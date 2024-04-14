de1c1003cd8d7f150bbfd84578e94a6cfe36d45e
Stéphane Lucien-Vauthier <slucienvauthier@icloud.com>
2024-04-14 20:54:56 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Stéphane Lucien-Vauthier on 14/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
