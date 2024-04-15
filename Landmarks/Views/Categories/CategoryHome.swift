544cd4e82470dcb8bfd7145b90f6481d7e44f60a
Stéphane Lucien-Vauthier <slucienvauthier@icloud.com>
2024-04-15 09:36:04 +0200

    Section 1 : Add a category view

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationSplitView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    CategoryHome()
+}
